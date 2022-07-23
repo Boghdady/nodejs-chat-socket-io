43ac76277c08ad1b8c5797fe42a5a0b6103787e4
boghdady <boghdady107@gmail.com>
2022-07-23 17:53:32 +0200

    Create chat ui

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
+// serve static files
+app.use(express.static('public'));
+// serve template engine
 app.set('view engine', 'ejs');
 
 const port = process.env.PORT || 3000;
