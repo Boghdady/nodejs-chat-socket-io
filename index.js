3ad913b7bb666407de92b5afcb354ca38e2bf839
boghdady <boghdady107@gmail.com>
2022-07-24 02:05:10 +0200

    emit event with data and handling event using on method

--- index.js
+++ index.js
@@ -14,6 +14,14 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+io.on('connection', (socket) => {
+  // on: receive data from client
+  socket.on('btnData', (data) => {
+    // emit: send data to client
+    socket.emit('serverEvent', 'data from the server');
+  });
+});
+
 server.listen(port, () => {
   console.log(`Server running at: http://localhost:${port}`);
 });
