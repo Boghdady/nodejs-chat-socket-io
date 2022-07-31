84ea39d31320e66e97d77d26c2f16fecdbcfdbb5
boghdady <boghdady107@gmail.com>
2022-07-31 12:57:27 +0200

    prepate private chatroom project

---
+++ private_chat_room/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+//  server static files
+app.use(express.static('public'));
+// serve template engine
+app.set('view engine', 'ejs');
+
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log(`server running on : http://localhost:${port}`);
+});
